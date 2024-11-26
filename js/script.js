e532df53c3fd10e45e25333f6fc77feb7ea2092d
My Name <umaransari4444r@gmail.com>
2024-11-26 12:18:27 +0530

    portfolio commit

---
+++ js/script.js
@@ -0,0 +1,11 @@
+function visitGitHub() {
+    window.open("https://github.com/Umarroxx")
+  }
+
+function visitLinkedIn() {
+  window.open("https://www.linkedin.com/in/mohd-umar-ansari-6a420425b/")
+}
+
+function visitInstagram(){
+  window.open("https://www.instagram.com/umar__roxx/")
+}
